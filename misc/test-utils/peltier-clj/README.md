2455a1f15c5f051cd3e6f36cd18e14847b73496f
niyarin <niyarium@gmail.com>
2026-02-02 08:20:19 +0900

    chore: Add freeze command to peltier-clj.

--- misc/test-utils/peltier-clj/README.md
+++ misc/test-utils/peltier-clj/README.md
@@ -4,6 +4,8 @@ Clojure implementation of peltier for comparison/benchmarking.
 
 ## Usage
 
+### Thaw (Nippy → EDN)
+
 ```bash
 # Convert Nippy to EDN
 clojure -M:run thaw input.nippy
@@ -13,14 +15,35 @@ clojure -M:run thaw -p input.nippy
 
 # Output to file
 clojure -M:run thaw -o output.edn input.nippy
-
-# Help
-clojure -M:run thaw --help
 ```
 
-## Example
+### Freeze (EDN → Nippy)
+
+```bash
+# Convert EDN to Nippy
+clojure -M:run freeze input.edn -o output.nippy
+
+# Write to stdout (binary)
+clojure -M:run freeze input.edn > output.nippy
+```
+
+### Help
+
+```bash
+clojure -M:run --help
+```
+
+## Examples
 
 ```bash
 cd misc/test-utils/peltier-clj
+
+# Thaw: Nippy to EDN
 clojure -M:run thaw ../../../benchmark_nocomp.nippy -o /tmp/output.edn
+
+# Freeze: EDN to Nippy
+clojure -M:run freeze /tmp/output.edn -o /tmp/test.nippy
+
+# Round-trip test
+clojure -M:run thaw /tmp/test.nippy
 ```
--- misc/test-utils/peltier-clj/src/peltier/core.clj
+++ misc/test-utils/peltier-clj/src/peltier/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]]
             [clojure.tools.cli :refer [parse-opts]]
+            [clojure.edn :as edn]
             [taoensso.nippy :as nippy])
   (:gen-class))
 
@@ -22,12 +23,24 @@
       (spit output-file edn-str)
       (print edn-str))))
 
+(defn freeze-edn [input-file output-file]
+  (let [edn-data (edn/read-string (slurp input-file))
+        nippy-bytes (nippy/freeze edn-data {:compressor nil})]
+    (if output-file
+      (with-open [out (io/output-stream output-file)]
+        (.write out nippy-bytes))
+      (.write System/out nippy-bytes))))
+
 (defn -main [& args]
   (let [{:keys [options arguments summary errors]} (parse-opts args cli-options)]
     (cond
       (:help options)
       (do
-        (println "Usage: peltier thaw [options] <input.nippy>")
+        (println "Usage: peltier <thaw|freeze> [options] <input-file>")
+        (println)
+        (println "Commands:")
+        (println "  thaw    Decode Nippy data to EDN")
+        (println "  freeze  Encode EDN data to Nippy")
         (println)
         (println "Options:")
         (println summary)
@@ -41,24 +54,25 @@
 
       (empty? arguments)
       (do
-        (println "Usage: peltier thaw [options] <input.nippy>")
+        (println "Usage: peltier <thaw|freeze> [options] <input-file>")
         (System/exit 1))
 
       :else
       (let [command (first arguments)]
-        (case command
-          "thaw"
-          (if-let [input-file (second arguments)]
-            (try
+        (if-let [input-file (second arguments)]
+          (try
+            (case command
+              "thaw"
               (thaw-nippy input-file (:output options) (:pretty options))
-              (catch Exception e
-                (println "Error:" (.getMessage e))
+              "freeze"
+              (freeze-edn input-file (:output options))
+              (do
+                (println "Unknown command:" command)
+                (println "Available commands: thaw, freeze")
                 (System/exit 1)))
-            (do
-              (println "Error: No input file specified")
+            (catch Exception e
+              (println "Error:" (.getMessage e))
               (System/exit 1)))
-
           (do
-            (println "Unknown command:" command)
-            (println "Available commands: thaw")
+            (println "Error: No input file specified for command" command)
             (System/exit 1)))))))
